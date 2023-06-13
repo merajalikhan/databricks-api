ad84efcc4a87476e10cc1b238b0a3831b2d6376e
merajalikhan <104003533+merajalikhan@users.noreply.github.com>
2023-06-13 14:13:09 +0100

    Create connect.py

---
+++ src/connect.py
@@ -0,0 +1,7 @@
+from databricks_api import DatabricksAPI
+
+# Provide a host and token
+db = DatabricksAPI(
+    host="https://adb-7772147484121042.2.azuredatabricks.net",
+    token="dapif71015e730f71f5e672c5dac3024d8ec"
+)
