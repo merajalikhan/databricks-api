79db3c44e108340c23040d2072d8a20bc07fd83a
merajalikhan <104003533+merajalikhan@users.noreply.github.com>
2023-06-13 15:11:38 +0100

    Update connect.py

--- src/connect.py
+++ src/connect.py
@@ -16,7 +16,7 @@ for job in jobs_list['jobs']:
   #print(f"{job.settings['job_name']}: {job['job_id']}")
    print(f"{job['job_id']}")
 
-db.jobs.run_now(
+db.jobs.run_now()
     job_id="60062762225560"
 
 
