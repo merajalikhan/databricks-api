360c9b0ffd22e43f455f6182274d977392f780cc
merajalikhan <104003533+merajalikhan@users.noreply.github.com>
2023-06-13 14:20:55 +0100

    Update connect.py

--- src/connect.py
+++ src/connect.py
@@ -5,3 +5,12 @@ db = DatabricksAPI(
     host="https://adb-7772147484121042.2.azuredatabricks.net",
     token="dapif71015e730f71f5e672c5dac3024d8ec"
 )
+
+db.jobs.list_jobs(
+    job_type=None,
+    expand_tasks=None,
+    limit=None,
+    offset=None,
+    headers=None,
+    version=None,
+)
